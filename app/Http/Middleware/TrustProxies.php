80fb1df8951e3183a2870fc01aeab660800d0fb3
alt-num <ibigfeet0@gmail.com>
2025-12-13 17:01:42 +0800

    hope

---
+++ app/Http/Middleware/TrustProxies.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Illuminate\Http\Request;
+use Fideloper\Proxy\TrustProxies as Middleware;
+
+class TrustProxies extends Middleware
+{
+    // Trust all proxies (Railway uses dynamic proxy IPs)
+    protected $proxies = '*';
+
+    // Use full set of forwarded headers to correctly detect HTTPS
+    protected $headers =
+        Request::HEADER_X_FORWARDED_FOR |
+        Request::HEADER_X_FORWARDED_HOST |
+        Request::HEADER_X_FORWARDED_PORT |
+        Request::HEADER_X_FORWARDED_PROTO |
+        Request::HEADER_X_FORWARDED_AWS_ELB;
+}
